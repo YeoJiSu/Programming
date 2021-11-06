790fbcfcb4de0cdccecde7b2c93ceed4a0fcf95a
Kyungseo-Park <troublesome.dev@gmail.com>
2021-11-06 17:35:22 +0900

    이거 답이 뭐임?

---
+++ 1단계/5.py
@@ -0,0 +1,14 @@
+## 이거 답이 뭐임?
+
+def aaaaaaaa():
+    a = input()
+    b = input()
+
+    try:
+        print(int(a)+int(b))
+    except:
+        aaaaaaaa()
+
+
+if __name__ == "__main__":
+    aaaaaaaa()
