c87953031a5d713a0fae8cb1c5603e78d4737af9
YeoJiSu <duwltn1301@naver.com>
2021-11-07 14:18:35 +0900

    사칙연산

---
+++ python /9.py
@@ -0,0 +1,7 @@
+num_array=input()
+num=num_array.split(" ")
+print(int(num[0])+int(num[1]))
+print(int(num[0])-int(num[1]))
+print(int(num[0])*int(num[1]))
+print(int(int(num[0])/int(num[1])))
+print(int(num[0])%int(num[1]))
