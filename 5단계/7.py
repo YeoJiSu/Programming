c0bd5cc559138b66da5cf56f1a1a2e950421f7d4
YeoJiSu <duwltn1301@naver.com>
2021-11-17 17:41:01 +0900

    평균 넘는 학생 비율 계산

--- 5단계/7.py
+++ 5단계/7.py
@@ -1,1 +1,24 @@
-# 7
+import sys
+
+def main():
+    try:
+        classes = int(sys.stdin.readline())       
+        problem(classes)
+    except:
+        exit()
+        
+def problem(x):
+    for i in range(0,x):
+        each = list(map(int, sys.stdin.readline().split()))
+        print("{:.3f}%".format(round(rate(each),3)))
+
+def rate(x):
+    count = 0
+    ave = (sum(x)-x[0])/x[0]
+    for i in range(1, len(x)):
+        if x[i]>ave:
+            count+=1
+    return count/int(x[0])*100
+    
+        
+main()
