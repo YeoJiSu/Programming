a36e100c497d3a89dd994b5aadecc0a0e03c95cb
YeoJiSu <duwltn1301@naver.com>
2021-11-24 14:04:50 +0900

    벌집이 형성되는 규칙에 따라 벌집의 위치를 구하는 문제

--- 8단계/2.py
+++ 8단계/2.py
@@ -1,11 +1,14 @@
 import sys
 num = int(sys.stdin.readline())
 
-i = 1
-while True:
-    if num >= 2+ 3*(i-2)*(i-1):
-        if num <= 1+ 3*(i-1)*i:
-            break
-    i+=1
-print(i)
-# 이렇게 하면 시간초과 에러 발생함 
+if num==1:
+    print(1)
+else:
+    i = 1
+    while True:
+        if num >= 2+ 3*(i-2)*(i-1):
+            if num <= 1+ 3*(i-1)*i:
+                break
+        i+=1
+    print(i)
+# 이렇게 하면 시간초과 에러 발생함 -> 원인이 1을 넣었을 때 무한 루프 돌아서였음 
