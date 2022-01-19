6a0bc6677a62f36f1fbb21462647357eadaed181
YeoJiSu <duwltn1301@naver.com>
2022-01-19 14:16:14 +0900

    틀렸음.. 고쳐야함

---
+++ 9단계/11.py
@@ -0,0 +1,24 @@
+import sys
+import math
+def count_position(x1, y1, r1, x2, y2, r2):
+    if (x1, y1) == (x2, y2) and r1 == r2: # 일치하면 무한대 
+        return -1
+    len = math.sqrt((x1-x2)**2+ (y1-y2)**2) # 점과 점사이의 거리
+    if r1> r2:
+        max = r1
+    else:
+        max = r2
+        
+    if len == r1+r2 or len==int(max/2):# 한점에서 만나는 경우
+        return 1
+    elif len > r1+r2 or len < int(max/2): # 안만나는 경우
+        return 0
+    else: # 두점에서 만나는 경우
+        return 2
+    
+def main():
+    T = int(sys.stdin.readline())
+    for i in range(0,T):
+        x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split(" "))
+        print(count_position(x1, y1, r1, x2, y2, r2))
+main()
